efa8e09811b9d1cba3edd27894f308645fac5337
CresBloomoon <metroidm2421@gmail.com>
2023-01-29 16:43:17 +0900

    変更しました

---
+++ BlazorECommerce/Server/Extension/UserExtension.cs
@@ -0,0 +1,18 @@
+﻿using BlazorECommerce.Shared.Entities;
+using Microsoft.Graph;
+
+namespace BlazorECommerce.Server.Extentions
+{
+	public static class UserExtension
+	{
+		public static ShopUser ToShopUser(this User user)
+		{
+			return new ShopUser
+			{
+				Id = user.Id,
+				DisplayName = user.DisplayName,
+				MobilePhone = user.MobilePhone,
+			};
+		}
+	}
+}
---
+++ BlazorECommerce/Shared/Entities/ShopUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorECommerce.Shared.Entities
+{
+	public sealed class ShopUser
+	{
+		public string Id { get; set; }
+
+		[Required]
+		public string DisplayName { get; set; }
+
+		[Required]
+		public string MobilePhone { get; set; }
+	}
+}
