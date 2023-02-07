fc6ba5593cc7894a623567691ea1aba1fa4c4113
CresBloomoon <metroidm2421@gmail.com>
2023-02-07 21:30:47 +0900

    commited

---
+++ component_param_blazor/Models/Person.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace component_param_blazor.Models{
+    public class Person{
+        public int ID{get;set;}
+        public string FirstName{get;set;}
+        public string LastName{get;set;}
+    }
+}
